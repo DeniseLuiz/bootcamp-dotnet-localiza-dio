4fbbfd9c8aae0bd1e3570a1e2b1f0b33b79c6e27
DeniseLuiz <denisesantos.luiz@usp.br>
2021-04-18 20:23:50 -0300

    initial structure

--- 03-aplicacao/aplicacao/Program.cs
+++ 03-aplicacao/aplicacao/Program.cs
@@ -6,7 +6,46 @@ namespace aplicacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                    
+                        break;
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;    
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+ 
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1-Inserir novo aluno");
+            Console.WriteLine("2-Listar alunos");
+            Console.WriteLine("3-Calcular média geral");
+            Console.WriteLine("X-Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
