5dcb3d6c1255c00be382ced3b500669e6881dd10
DeniseLuiz <denisesantos.luiz@usp.br>
2021-05-08 22:02:28 -0300

    migrations

---
+++ mvc/mvc/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CursoMvc;Integrated Security=True");
+        }
+    }
+}
